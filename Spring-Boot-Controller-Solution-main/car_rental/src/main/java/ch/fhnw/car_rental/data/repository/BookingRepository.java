cda86678257f87d8fe6a1cc1f991555b2be62b53
Lebus004 <119622273+Lebus004@users.noreply.github.com>
2025-04-12 13:53:56 +0000

    Updated CarController: Added car list functionality and improved endpoints

---
+++ Spring-Boot-Controller-Solution-main/car_rental/src/main/java/ch/fhnw/car_rental/data/repository/BookingRepository.java
@@ -0,0 +1,14 @@
+package ch.fhnw.car_rental.data.repository;
+
+import ch.fhnw.data.domain.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+//JpaRepository should be typed to the domain class and an ID type
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Booking findByBooking_id(Integer Booking_id);
+    Booking addBooking(Booking booking);
+
+}
