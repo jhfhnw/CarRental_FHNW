38a60ee92bf84217c8aaee7f4cc8ac5b646c1ed5
Lebus004 <119622273+Lebus004@users.noreply.github.com>
2025-04-28 13:18:03 +0000

    New folder and java file for security added, not functional right now

---
+++ Spring-Boot-Controller-Solution-main/car_rental/src/main/java/ch/fhnw/car_rental/security/Security.java
@@ -0,0 +1,10 @@
+package ch.fhnw.car_rental.security;
+
+import ch.fhnw.car_rental.data.domain.Customer;
+import ch.fhnw.car_rental.data.domain.Admin;
+
+
+// security file für Authentifizierung und Autorisierung von Customers und Admins
+public class Security {
+
+}
